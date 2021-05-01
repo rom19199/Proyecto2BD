d9ae9a9fb48a075c0bcba198a29e27b31183b30d
rom19199 <54779748+rom19199@users.noreply.github.com>
2021-05-01 12:11:12 -0600

    Djando Table

---
+++ Proyecto2/forms.py
@@ -0,0 +1,13 @@
+from django import forms  
+from Proyecto2.models import Music  
+
+class MusicForm(forms.ModelForm):  
+    class Meta:  
+        model = Music
+        fields = ['name', 'album', 'artista', 'year','genero'] #https://docs.djangoproject.com/en/3.0/ref/forms/widgets/
+        widgets = { 'name': forms.TextInput(attrs={ 'class': 'form-control' }), 
+                    'album': forms.TextInput(attrs={ 'class': 'form-control' }),
+                    'artista': forms.TextInput(attrs={ 'class': 'form-control' }),
+                    'year': forms.TextInput(attrs={ 'class': 'form-control' }),
+                    'genero': forms.TextInput(attrs={ 'class': 'form-control' }),
+      }
---
+++ Proyecto2/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Music(models.Model):  
+    name = models.CharField(max_length=100)  
+    album = models.CharField(max_length=100)  
+    artista = models.CharField(max_length=100)  
+    year = models.CharField(max_length=100)  
+    genero = models.CharField(max_length=100)  
+  
+    class Meta:  
+        db_table = "songs"
+        # app_label="Proyecto2"
