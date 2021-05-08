f68ebc908da7a6294db7ad3bd2b2c8488e52c069
rom19199 <54779748+rom19199@users.noreply.github.com>
2021-05-08 16:27:31 -0600

    Add files via upload

---
+++ tableuser/tableuser/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect  
+from tableuser.forms import MusicForm  
+from tableuser.models import Music
+
+# Create your views here.  
+def addnew(request):  
+    if request.method == "POST":  
+        form = MusicForm(request.POST)  
+        if form.is_valid():  
+            try:  
+                form.save()  
+                return redirect('/')  
+            except:  
+                pass 
+    else:  
+        form = MusicForm()  
+    return render(request,'index.html',{'form':form})  
+def index(request):  
+    songs = Music.objects.all()  
+    return render(request,"show.html",{'songs':songs})  
+
+def edit(request, id):  
+    song = Music.objects.get(id=id)  
+    return render(request,'edit.html', {'song':song})  
+
+def update(request, id):  
+    song = Music.objects.get(id=id)  
+    form = MusicForm(request.POST, instance = song)  
+    if form.is_valid():  
+        form.save()  
+        return redirect("/")  
+    return render(request, 'edit.html', {'song': song})  
+def destroy(request, id):  
+    song = Music.objects.get(id=id)  
+    song.delete()  
+    return redirect("/")  
